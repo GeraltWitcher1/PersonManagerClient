b43220ad9879e4fa03ba70b98814f18d63b81fa3
GeraltWitcher1 <57898274+GeraltWitcher1@users.noreply.github.com>
2021-10-06 17:35:05 +0200

    Adult manager implemented

---
+++ DNPblazorAssignment/Data/AdultManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FileData;
+using Models;
+
+namespace DNPblazorAssignment.Data
+{
+    
+    public class AdultManager : IAdultManager
+    {
+        private FileContext fileContext;
+        private IList<Adult> adults;
+        
+        public AdultManager()
+        {
+            fileContext = new FileContext();
+            adults = fileContext.Adults;
+        }
+        
+        public void AddAdult(Adult adult)
+        {
+            int max;
+            try
+            {
+                max = adults.Max(ad => ad.Id);
+            }
+            catch (InvalidOperationException)
+            {
+                max = 1;
+            }
+            
+            adult.Id += ++max;
+            adults.Add(adult);
+            fileContext.SaveChanges();
+        }
+
+        public void RemoveAdult(int id)
+        {
+            adults.Remove(GetAdult(id));
+            fileContext.SaveChanges();
+        }
+
+        public void UpdateAdult(Adult adult)
+        {
+            Adult toUpdate = GetAdult(adult.Id);
+            toUpdate.FirstName = adult.FirstName;
+            toUpdate.LastName = adult.LastName;
+            toUpdate.Age = adult.Age;
+            toUpdate.Weight = adult.Weight;
+            toUpdate.Height = adult.Height;
+            toUpdate.HairColor = adult.HairColor;
+            fileContext.SaveChanges();
+        }
+
+        public Adult GetAdult(int id)
+        {
+            return adults.First(ad => ad.Id == id);
+        }
+
+        public IList<Adult> GetAll()
+        {
+            return new List<Adult>(adults);
+        }
+    }
+}
---
+++ DNPblazorAssignment/Data/IAdultManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace DNPblazorAssignment.Data
+{
+    public interface IAdultManager
+    {
+        
+        void AddAdult(Adult adult);
+        void RemoveAdult(int id);
+        void UpdateAdult(Adult adult);
+        Adult GetAdult(int id);
+        IList<Adult> GetAll();
+    }
+}
